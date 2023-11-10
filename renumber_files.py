818ccb0e126576338fb9d65ec4252245f1c8973d
Bruce Eckel <MindviewInc@gmail.com>
2023-11-09 17:04:03 -0800

    Partial rename

--- renumber_files.py
+++ renumber_files.py
@@ -3,17 +3,16 @@ import re
 from pathlib import Path
 import sys
 from typing import Generator, Tuple, List
-from pprint import pprint
 
 
-def renumber_files(files: List[Path]) -> Generator[Tuple[str, str], None, None]:
+def renumber_files(files: List[Path]) -> Generator[Tuple[Path, Path], None, None]:
     # Sort files by number while handling missing numbers and the '!' marker
     def file_sort_key(file: Path) -> int:
         match = re.match(r"(\d+)", file.stem)
         return int(match.group(1)) if match else float("inf")
 
     files_sorted = sorted(files, key=file_sort_key)
-    pprint(files_sorted)
+    new_files_list = []
 
     # Find new file position and name
     new_file = None
@@ -34,23 +33,32 @@ def renumber_files(files: List[Path]) -> Generator[Tuple[str, str], None, None]:
 
     priority_number = int(priority_match.group(1))
 
-    # Determine the new numbering for files
+    # Create a list of tuples for renaming
     for file in files_sorted:
         current_number_match = re.match(r"(\d+)", file.stem)
         if current_number_match:
             current_number = int(current_number_match.group(1))
-            # If this file is the new file, yield its new name without the '!'
             if file == new_file:
+                # New file takes the priority spot
                 new_file_name = f"{priority_number}. {file.stem.split('! ', 1)[1]}.md"
-                yield (file.name, new_file_name)
-            # If the current number is greater than or equal to the priority number, increment it
+                new_files_list.append((file, file.with_name(new_file_name)))
             elif current_number >= priority_number:
+                # Increment the file number
                 new_number = current_number + 1
                 new_file_name = f"{new_number}. {file.stem.split('. ', 1)[1]}.md"
-                yield (file.name, new_file_name)
+                new_files_list.append((file, file.with_name(new_file_name)))
+
+    # Yield the new and old names after creating the complete list
+    for old_file, new_file in new_files_list:
+        yield (old_file, new_file)
 
 
 files = list(Path(".").glob("*.md"))
-for old_name, new_name in renumber_files(files):
-    print(f"Rename '{old_name}' to '{new_name}'")
-    os.rename(old_name, new_name)
+renaming_operations = list(renumber_files(files))
+
+# Perform the renaming operations
+for old_file, new_file in renaming_operations:
+    print(f"Rename '{old_file.name}' to '{new_file.name}'")
+    # os.rename(old_file, new_file)
+
+print("Files have been renumbered successfully.")
