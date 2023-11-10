1113082b33b4b61b46ba3913bcd538200a1efc93
Bruce Eckel <MindviewInc@gmail.com>
2023-11-09 18:36:02 -0800

    Update renumber_files.py

--- renumber_files.py
+++ renumber_files.py
@@ -1,64 +1,84 @@
-import os
-import re
 from pathlib import Path
-import sys
-from typing import Generator, Tuple, List
+from typing import Generator, Tuple
+import re
+from pprint import pprint
+from dataclasses import dataclass, field
+import operator
 
 
-def renumber_files(files: List[Path]) -> Generator[Tuple[Path, Path], None, None]:
-    # Sort files by number while handling missing numbers and the '!' marker
-    def file_sort_key(file: Path) -> int:
-        match = re.match(r"(\d+)", file.stem)
-        return int(match.group(1)) if match else float("inf")
+@dataclass(order=True)
+class NumberedFile:
+    sort_index_1: int = field(init=False)
+    sort_index_2: str = field(init=False)
+    original_name: str
+    text_name: str
+    number: int
+    priority: str
+    new_name: str = ""
 
-    files_sorted = sorted(files, key=file_sort_key)
-    new_files_list = []
-
-    # Find new file position and name
-    new_file = None
-    for file in files_sorted:
-        if "!" in file.stem:
-            new_file = file
-            break
-
-    if not new_file:
-        print("No new file with '!' in its name found.")
-        sys.exit()
-
-    # Extract the priority number from the new file name
-    priority_match = re.match(r"(\d+)\.!", new_file.stem)
-    if not priority_match:
-        print(f"'{new_file}' does not have a proper numeric priority.")
-        sys.exit()
-
-    priority_number = int(priority_match.group(1))
-
-    # Create a list of tuples for renaming
-    for file in files_sorted:
-        current_number_match = re.match(r"(\d+)", file.stem)
-        if current_number_match:
-            current_number = int(current_number_match.group(1))
-            if file == new_file:
-                # New file takes the priority spot
-                new_file_name = f"{priority_number}. {file.stem.split('! ', 1)[1]}.md"
-                new_files_list.append((file, file.with_name(new_file_name)))
-            elif current_number >= priority_number:
-                # Increment the file number
-                new_number = current_number + 1
-                new_file_name = f"{new_number}. {file.stem.split('. ', 1)[1]}.md"
-                new_files_list.append((file, file.with_name(new_file_name)))
-
-    # Yield the new and old names after creating the complete list
-    for old_file, new_file in new_files_list:
-        yield (old_file, new_file)
+    def __post_init__(self):
+        self.sort_index_1 = self.number
+        self.sort_index_2 = self.priority
 
 
-files = list(Path(".").glob("*.md"))
-renaming_operations = list(renumber_files(files))
+original_files = Path(".").glob("*.md")
+numbered_files = []
+for file in original_files:
+    num = re.match(r"(\d+)\.", file.stem)
+    if not num:
+        continue
+    else:
+        file_number = int(num.group(1))
+        text_name = file.name
+        priority = "B"
+        if "!" in text_name:
+            priority = "A"
+            text_name = text_name.replace("!", "", 1)
+        text_name = text_name.split(".", 1)[1].strip()
+        numbered_files.append(NumberedFile(file.name, text_name, file_number, priority))
 
-# Perform the renaming operations
-for old_file, new_file in renaming_operations:
-    print(f"Rename '{old_file.name}' to '{new_file.name}'")
-    # os.rename(old_file, new_file)
+for i, nf in enumerate(sorted(numbered_files)):
+    nf.new_name = f"{i}. {nf.text_name}"
+    if nf.original_name != nf.new_name:
+        print(f"rename '{nf.original_name}' to '{nf.new_name}'")
 
-print("Files have been renumbered successfully.")
+# def renumber_files(directory: Path) -> Generator[Tuple[str, str], None, None]:
+#     # Gather all files and sort by name
+#     files = sorted(directory.glob("*.md"), key=lambda f: f.stem)
+#     pprint(files)
+#     pivot_file = None
+#     pivot_number = None
+
+#     # Find the pivot file and determine the pivot number
+#     for file in files:
+#         if "!" in file.stem:
+#             pivot_file = file
+#             pivot_number = int(re.match(r"(\d+)\.!", file.stem).group(1))
+#             break
+
+#     if pivot_file is None:
+#         raise ValueError("No pivot file with '!' in its name found.")
+
+#     # Generate new file names up to the pivot
+#     for i, file in enumerate(files[: files.index(pivot_file)]):
+#         new_name = f"{i}. {file.stem.split('. ', 1)[1]}.md"
+#         yield (file.name, new_name)
+
+#     # Generate new name for the pivot file
+#     new_pivot_name = f"{pivot_number}. {pivot_file.stem.split('! ', 1)[1]}.md"
+#     yield (pivot_file.name, new_pivot_name)
+
+#     # Generate new file names after the pivot
+#     start_after_pivot = pivot_number + 1
+#     for i, file in enumerate(
+#         files[files.index(pivot_file) + 1 :], start=start_after_pivot
+#     ):
+#         new_name = f"{i}. {file.stem.split('. ', 1)[1]}.md"
+#         yield (file.name, new_name)
+
+
+# # Example usage
+# for old_name, new_name in renumber_files(Path(".")):
+#     print(f"Rename '{old_name}' to '{new_name}'")
+#     # Uncomment the line below to actually perform the renaming
+#     # os.rename(directory_path / old_name, directory_path / new_name)
