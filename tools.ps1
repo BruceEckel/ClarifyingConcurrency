4d2d64ebb9af1bb5c2278979e93634ff3adf2e99
Bruce Eckel <MindviewInc@gmail.com>
2023-11-13 22:46:50 -0800

    Update tools.ps1

--- tools.ps1
+++ tools.ps1
@@ -6,10 +6,8 @@ param(
 
 if ($arg) {
     # If an argument is provided, run tools.py with the argument
-    # python .\markdown_utils\tools.py $arg
-    rye run tools $arg
+    Clear-Host; rye run tools $arg
 } else {
     # If no argument is provided, display the help for tools.py
-    # python .\markdown_utils\tools.py --help
-    rye run tools --help
+    Clear-Host; rye run tools --help
 }
