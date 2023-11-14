605965289a3b2c1978e07febb75b0769ec1f5643
Bruce Eckel <MindviewInc@gmail.com>
2023-11-13 22:56:07 -0800

    cleanup

--- markdown_utils/tools.py
+++ markdown_utils/tools.py
@@ -1,5 +1,10 @@
 import typer
-from markdown_utils import separator, check_code_listings, check_markdown, NumberedFile
+from markdown_utils import (
+    separator,
+    check_code_listings,
+    check_markdown,
+    NumberedFile,
+)
 from pathlib import Path
 
 app = typer.Typer()
@@ -22,13 +27,19 @@ def listings():
 
 @app.command()
 def renumber():
-    for chapter in NumberedFile.chapters().files:
-        print(chapter)
-        # print(f"'{chapter.original_name}'  -->  '{chapter.new_name}'")
+    chapter_changes = NumberedFile.chapters().changes
+    appendix_changes = NumberedFile.appendices().changes
+    if not chapter_changes and not appendix_changes:
+        print("No Changes")
+    for chapter in NumberedFile.chapters().changes:
+        # print(chapter)
+        print(f"'{chapter.original_name}'  -->  '{chapter.new_name}'")
         # os.rename(chapter.original_name, chapter.new_name)
-    for appendix in NumberedFile.appendices().files:
-        print(appendix)
-        # print(f"'{appendix.original_name}'  -->  '{appendix.new_name}'")
+    for appendix in NumberedFile.appendices().changes:
+        # print(appendix)
+        print(
+            f"'{appendix.original_name}'  -->  '{appendix.new_name}'"
+        )
         # os.rename(appendix.original_name, appendix.new_name)
 
 
